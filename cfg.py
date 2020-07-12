e751cfc312e336e3d9ca866b77238232d599a0be
_____ <spark.did.a.code@gmail.com>
2020-07-11 20:33:39 -0700

    Replace game configs with YAML

--- cfg.py
+++ cfg.py
@@ -3,11 +3,11 @@ import yaml
 import cmds
 
 #game
-currentGameConfig = "pubg.csv"
+currentGameConfig = "pubg.yml"
 
 #channel point identifiers - when a user redeems channel points, the channel point will have an identifier (e.g. "b3df3074-eb94-4f44-811a-1815835edfdd")
 channelPoint1 = ""
 
 with open(currentGameConfig) as f:
-    config = yaml.load(f)
+    config = yaml.safe_load(f)
     cmds.from_config(config)
